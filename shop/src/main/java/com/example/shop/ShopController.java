56474e90a1191f78b635b5bbc71db4089599b53b
gruchh <kubol2@gmail.com>
2024-11-10 20:38:20 +0100

    Service registry + api gateway + shop + warehouse adjust

--- shop/src/main/java/com/example/shop/ShopController.java
+++ shop/src/main/java/com/example/shop/ShopController.java
@@ -2,9 +2,11 @@ package com.example.shop;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/shop")
 public class ShopController {
 
     @Value("${eureka.instance.instance-id}")
