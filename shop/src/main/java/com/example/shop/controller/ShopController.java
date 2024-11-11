b360ee02efd4ba74df623ce97d7d8050c6c2a66c
gruchh <kubol2@gmail.com>
2024-11-11 14:30:56 +0100

    Service registry + api gateway + shop + warehouse adjust

---
+++ shop/src/main/java/com/example/shop/controller/ShopController.java
@@ -0,0 +1,34 @@
+package com.example.shop.controller;
+
+import com.example.shop.model.Part;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shop")
+public class ShopController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello World " + instanceId;
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/parts")
+    public List<Part> partList() {
+        List<Part> partList = List.of(new Part(1L, "Part 1", "Producer1", "https://www.creativefabrica.com/wp-content/uploads/2022/12/14/a-piece-of-cheese-cartoon-ilustration-Graphics-52238740-1.jpg"),
+                new Part(2L, "Part 2", "Producer 2", "https://www.creativefabrica.com/wp-content/uploads/2022/12/14/a-piece-of-cheese-cartoon-ilustration-Graphics-52238740-1.jpg"));
+
+        return partList;
+    }
+
+
+}
---
+++ shop/src/main/java/com/example/shop/model/Part.java
@@ -0,0 +1,5 @@
+package com.example.shop.model;
+
+public record Part (Long id, String name, String producer, String imageUrl) {
+
+}
